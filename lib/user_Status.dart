7389af572c4747a9afb5944485b6fe58442b59ea
Prince Tomar <71916212+princetomar@users.noreply.github.com>
2023-06-30 02:52:42 +0530

    Update user_Status.dart

--- lib/user_Status.dart
+++ lib/user_Status.dart
@@ -6,9 +6,6 @@ class UserStatusWidget extends StatefulWidget {
   _UserStatusWidgetState createState() => _UserStatusWidgetState();
 }
 
-// const String authToken =
-//     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OWQ0MWE2Y2Q3ZmRmYjVmZmVlZDRkMSIsInVzZXJUeXBlIjoiZG9jdG9yIiwiaWF0IjoxNjg4MDI3NTU5LCJleHAiOjE2ODg4OTE1NTl9.g7eaSWVQsdvDOjwlGWbim_31Y4KKCxz03tVzuhYI7qU';
-
 class _UserStatusWidgetState extends State<UserStatusWidget> {
   bool isOnline = false;
   // late IO.Socket socket;
